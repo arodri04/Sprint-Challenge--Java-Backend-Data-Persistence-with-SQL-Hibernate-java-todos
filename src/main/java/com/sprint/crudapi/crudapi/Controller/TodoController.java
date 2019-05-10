2ddd1fe29c693781dd03afaf9964d4803884cec7
arodri04 <augustine.rodriguez55@gmail.com>
2019-05-10 13:15:38 -0600

    Added a delete

--- src/main/java/com/sprint/crudapi/crudapi/Controller/TodoController.java
+++ src/main/java/com/sprint/crudapi/crudapi/Controller/TodoController.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/todo")
@@ -29,6 +29,20 @@ public class TodoController
       Authentication userAuth = SecurityContextHolder.getContext().getAuthentication();
       currentuser = userAuth.getName();
       User user = userService.findUserName(currentuser);
-      return new ResponseEntity<>(user, HttpStatus.OK);
+      return new ResponseEntity<>(user.getTodos(), HttpStatus.OK);
+   }
+
+   @PostMapping(value = "/users", consumes="application/json", produces = "application/json")
+   public ResponseEntity<?> addUser(@Valid @RequestBody User user)
+   {
+      User temp = userService.save(user);
+      return new ResponseEntity<>(temp, HttpStatus.OK);
+   }
+
+   @DeleteMapping(value = "/users/delete/{id}")
+   public ResponseEntity<?> deleteuser(@PathVariable Long id)
+   {
+      userService.delete(id);
+      return new ResponseEntity<>("YEETED ID NUMBER "+id, HttpStatus.OK);
    }
 }
